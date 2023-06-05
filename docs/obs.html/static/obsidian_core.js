58b5cda1611c34a78e231ea900e8ba5f09c9485a
Inom T <i@inomoz.ru>
2023-06-06 02:05:12 +0600

    autopush

--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '325831705164590282435936376358435798722'                       // used to check whether the localStorage data is stale
+var gzip_hash = '106318454231893566290545573972866625771'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
